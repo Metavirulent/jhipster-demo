1cc821f542b29b3bd0d0cd57bc9e1d28014105f9
Markus Öllinger <oellinger.markus@gmail.com>
2015-09-23 19:02:58 +0200

    modified theme

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -3,11 +3,14 @@
 angular.module('demoApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV) {
         $scope.isAuthenticated = Principal.isAuthenticated;
+        $scope.account = Principal.account;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
 
         $scope.logout = function () {
             Auth.logout();
+            $scope.isAuthenticated=false;
             $state.go('home');
         };
+
     });
